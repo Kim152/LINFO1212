411311c308913576f213a007acdfc64790adebb3
Kim <kimberly.sanchez@student.uclouvain.be>
2020-11-10 15:59:10 +0100

    organization total

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express');
-var consolidate = require('consolidate')
+const path = require('path'); //me ayuda a manejar las rutas del servidor 
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose'); //permet de manipuler mongo db
+const passport = require('passport'); // permettre identifier un user
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const app = express ();
+const {url} = require('.database');
+mongoose.connect(url,{
+    
+})
+// configurations
+app.use(express.static('WEB')) 
 
-app.engine('html',consolidate.hogan)
-app.set('views','WEB')
 
-app.get('/question',function(req,res,next){
-    res.render('index.html',{year:req.query.year});
-});
+// Middlewares
 
-app.get('/nouvelincident',function(req,res,next){
-    res.render('Page2.html');
-});
 
-app.get('/registre',function(req,res,next){
-    res.render('identification.html');
-});
+//routes
 
-app.use(express.static('WEB'))
+// static files 
+
+// server run 
 app.listen(8080);
